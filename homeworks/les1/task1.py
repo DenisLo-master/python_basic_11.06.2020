a606fa8b3430fd9cf752e860561536fcbc3642c8
Denis <denis.lkg@gmial.com>
2020-06-15 02:17:21 +0300

    HW Les1 task1

--- homeworks/les1/task1.py
+++ homeworks/les1/task1.py
@@ -1,1 +1,21 @@
 "Home Work"
+
+greeting = 'Привет, незнакомец!!!'
+name = ''
+numbers = []
+count = 5
+i = count - 1
+
+print(greeting)
+name = input('Как тебя зовут?\n')
+print(f'Спасибо, {name}, а теперь введите пожалуйста {count} чисел')
+
+while i > -1:
+    number = input(f'Число №{count-i}:\n')
+    if number.isdigit():
+        numbers.append(number)
+        i -= 1
+        continue
+    print('!!!введите только число!!!')
+print(f'{name}, Вы ввели {numbers}')
+
