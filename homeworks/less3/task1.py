e00f5d9610225b028ab8432276193ae027abd39b
Denis <denis.lkg@gmial.com>
2020-06-20 14:32:31 +0300

    HW less1 task1

---
+++ homeworks/less3/task1.py
@@ -0,0 +1,33 @@
+"""1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль"""
+
+
+def division(dividend: int, divider: int) -> int:
+    """produces division
+
+    param: dividend, divider - int
+    :return: print result
+    """
+    try:
+        result = dividend / divider
+    except ZeroDivisionError:
+        print('Ошибка деления на 0')
+        exit()
+    print(result)
+
+
+while True:
+    try:
+        number1 = int(input('Введите делимое: '))
+        break
+    except ValueError:
+        print('Введите целое число')
+        continue
+while True:
+    try:
+        number2 = int(input('Введите делитель: '))
+        break
+    except ValueError:
+        print('Введите целое число')
+        continue
+division(number1, number2)
