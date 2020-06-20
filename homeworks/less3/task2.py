340f7ced022d5200de7329b279eaf9f007488c28
Denis <denis.lkg@gmial.com>
2020-06-20 18:11:58 +0300

    HW less1 task3

--- homeworks/less3/task2.py
+++ homeworks/less3/task2.py
@@ -2,13 +2,16 @@
 город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
 Реализовать вывод данных о пользователе одной строкой."""
 
+
 def profile(name: str, surname: str, date_of_b: str, city: str, email: str, phone: int) -> str:
     """
     displays user profile in line
 
     :return: result all params in line
     """
-    result = f'Анкета поьзователя: {name}, {surname}, {date_of_b}, {city}, {email}, {phone}'
+    result = f'Анкета пользователя: {name}, {surname}, {date_of_b}, {city}, {email}, {phone}'
     return result
 
-print(profile(name='Den', surname='L', date_of_b='25.01.85', city='Saint-Peterburg', email='denis.lkg@gmail.com', phone='9261577467'))
+
+print(profile(name='Den', surname='L', date_of_b='25.01.85', city='Saint-Peterburg', email='denis.lkg@gmail.com',
+              phone='9261577467'))
