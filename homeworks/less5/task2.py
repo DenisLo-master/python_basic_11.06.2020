59c4e9a6f827100ddcd0ed918b27910fbaab9290
Denis <denis.lkg@gmial.com>
2020-06-29 00:53:20 +0300

    HW less5 task2

--- homeworks/less5/task2.py
+++ homeworks/less5/task2.py
@@ -6,9 +6,8 @@ with open('task2-file1.txt', 'r') as f:
     words= 0
     for line in f:
         lines += 1
-        for word in line:
+        for word in line.split(' '):
             words +=1
         print(f'Строка {lines}, количество слов в строке: {words}')
-
 print(f'Количество строк в файле: {lines}')
 
---
+++ homeworks/less5/task3.py
@@ -0,0 +1,25 @@
+"""3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов. Определить,
+кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников."""
+
+user_list = []
+with open('task3-file1.txt', 'r') as f:
+    for line in f:
+        template = []
+        for word in line.split():
+            template.append(word)
+        user_list.append(template)
+print(user_list)
+
+low_salary = 20000
+i = 0
+sum_salary = 0
+avr = 0
+print('-' * 34)
+print(f'Сотрудники c окладом менее 20 000:')
+for i in range(len(user_list)):
+    if int(user_list[i][1]) < low_salary:
+        print(user_list[i][0])
+        sum_salary += int(user_list[i][1])
+        avr += 1
+print('-' * 34)
+print(f'Средний доход сотрудников: {int(sum_salary / avr)}')
