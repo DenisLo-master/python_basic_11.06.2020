71d29b80a8d11d514afef7376b7103b263799b2d
Denis <denis.lkg@gmial.com>
2020-07-13 03:13:47 +0300

    HW less8

--- homeworks/less8/task4-5-6.py
+++ homeworks/less8/task4-5-6.py
@@ -16,29 +16,25 @@ class Stock_Eq:
     __department_list = {1: 'Бухгалтерия', 2: 'Отдел продаж', 3: 'Маркетинговый отдел', 4: 'Клиентский отдел'}
     __stock_dict = {}
 
-    @property
-    def department(self):
-        return Stock_Eq.__department_list
+    @classmethod
+    def department(cls):
+        return cls.__department_list
 
-    @department.setter
-    def department(self, name):
-        Stock_Eq.__department_list[len(Stock_Eq.__department_list) + 1] = name
+    @classmethod
+    def department_inn(cls, name):
+        cls.__department_list[len(cls.__department_list) + 1] = name
 
-    @property
-    def equip(self):
-        return Office_equipment.equip
+    @classmethod
+    def stock(cls):
+        return cls.__stock_dict
 
-    @property
-    def stock(self):
-        return Stock_Eq.__stock_dict
-
-    @stock.setter
-    def stock(self, atr):
+    @classmethod
+    def stock_inn(cls):
         stock_count = {}
         _index = None
         # если отдел уже есть
-        if st.department[dep_choise] in st.__stock_dict.keys():
-            stock_count = st.__stock_dict[st.department[dep_choise]]
+        if cls.department()[dep_choise] in cls.__stock_dict.keys():
+            stock_count = cls.__stock_dict[cls.department()[dep_choise]]
             _key_equp = Office_equipment.equip[equip_choise]
             # если оборудование уже есть в отделе
             if _key_equp in stock_count.keys():
@@ -48,10 +44,7 @@ class Stock_Eq:
                 stock_count[_key_equp] = count_choise
         # если отдела еще нет
         else:
-            st.__stock_dict[st.department[dep_choise]] = {Office_equipment.equip[equip_choise]: count_choise}
-
-    def __str__(self):
-        return st.__stock_dict
+            cls.__stock_dict[cls.department()[dep_choise]] = {Office_equipment.equip[equip_choise]: count_choise}
 
 
 class DepartmentError(Exception):
@@ -84,73 +77,94 @@ class Office_equipment:
         Office_equipment.__idq += 1
         Office_equipment.equip[Office_equipment.__idq] = tuple(self.__str__().split(', '))
 
-    @
+    @staticmethod
+    def info():
+        for _k, _i in Office_equipment.equip.items():
+            print(_k, ":", _i)
+        print('-' * 50)
 
 
 class Printer(Office_equipment):
-    type = ''
-
     def __init__(self, name, color, type):
         Printer.type = type
         super().__init__(name, color)
+
+    @property
+    def type(self):
+        return self.type
+
+    @type.setter
+    def type(self, val):
+        self.type = val
 
     def __str__(self):
         return f'{self.__class__.__name__}, {self.name}, цвет:{self.color}, тип:{self.type}'
 
 
 class Scanner(Office_equipment):
-    format = ''
-
     def __init__(self, name, color, format):
         Scanner.format = format
         super().__init__(name, color)
+
+    @property
+    def format(self):
+        return self.format
+
+    @format.setter
+    def format(self, val):
+        self.format = val
 
     def __str__(self):
         return f'{self.__class__.__name__}, {self.name}, цвет:{self.color}, формат:{self.format}'
 
 
 class Copier(Office_equipment):
-    speed = ''
-
     def __init__(self, name, color, speed):
         Copier.speed = speed
         super().__init__(name, color)
 
+    @property
+    def speed(self):
+        return self.speed
+
+    @speed.setter
+    def speed(self, val):
+        self.speed = val
+
     def __str__(self):
-        return f'{self.__class__.__name__}, {self.name}, цвет:{self.color}, скорость:{self.speed}'
+        return f'{self.__class__.__name__}, {self.name}, цвет:{self.color}, скорость:{self.speed} л/мин'
 
 
-if __name__ == '__main__':
-    st = Stock_Eq()
-
-    # Примеры оборудования
-    p1 = Printer('Canon', 'black', 'b/w')
-    p2 = Printer('Samsung', 'white', 'color')
-    s1 = Scanner('MTR', 'red', 'A4')
-    s2 = Scanner('Xerox', 'black', 'A3')
-    c1 = Copier('Sams', 'blue', '15')
-    c2 = Copier('Can', 'gray', '60')
+# Примеры оборудования
+p1 = Printer('Canon', 'black', 'b/w')
+p2 = Printer('Samsung', 'white', 'color')
+s1 = Scanner('MTR', 'red', 'A4')
+s2 = Scanner('Xerox', 'black', 'A3')
+c1 = Copier('Sams', 'blue', '15')
+c2 = Copier('Can', 'gray', '60')
 
 # Работа в меню
 while True:
     try:
-        _input = input('Меню: 1 - показать склад, 2 - заказть оборудование: ')
-        if _input != '1' and _input != '2':
+        _input = input(
+            '\nМеню: 1 - показать склад, 2 - заказть оборудование, 3 - показать доступное оборудование:\n---->')
+        if _input != '1' and _input != '2' and _input != '3':
             raise MenuError
         # Отобразить продукцию на складе
         elif _input == '1':
-            if st.stock == {}:
+            if Stock_Eq.stock() == {}:
                 print('\nНа складе ничего нет')
-            for _k, _i in st.stock.items():
+                continue
+            for _k, _i in Stock_Eq.stock().items():
                 print('\n', _k)
                 for _ki, _ii in _i.items():
-                    print(f'{_ki}: {_ii}шт')
+                    print(f'{_ki}: {_ii} шт')
             print('_' * 50)
             continue
         # заказ оборудования на склад для отдела...
         elif _input == '2':
             # Выбор отдела из списка или создание нового с выбором
-            print('\n', st.department)
+            print('\n', Stock_Eq.department())
             while True:
                 try:
                     _input = input('Выберете отдел(код или введите "новый"): ')
@@ -158,10 +172,10 @@ while True:
                         raise DepartmentError
                     elif _input == 'новый':
                         _input = input('\nВведите название нового отдела: ')
-                        st.department = _input
-                        dep_choise = [key for key, itm in st.department.items() if itm == _input][0]
+                        Stock_Eq.department_inn(_input)
+                        dep_choise = [key for key, itm in Stock_Eq.department().items() if itm == _input][0]
                         break
-                    elif not int(_input) in st.department.keys():
+                    elif not int(_input) in Stock_Eq.department().keys():
                         raise DepartmentError
                     else:
                         dep_choise = int(_input)
@@ -170,7 +184,7 @@ while True:
                     print('\n', e)
 
             # Выбор оборудования из списка
-            print('\n')
+            print('_' * 50)
             for _k, _i in Office_equipment.equip.items():
                 print(f'{_k}: {_i}')
             while True:
@@ -200,8 +214,10 @@ while True:
                 except CountError as e:
                     print('\n', e)
 
-            st.stock = 'new'
+            Stock_Eq.stock_inn()
             print('_' * 50)
             continue
+        elif _input == '3':
+            Office_equipment.info()
     except MenuError as e:
         print('\n', e)
